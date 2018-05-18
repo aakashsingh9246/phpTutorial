b95d32c804ef6b231642468f070e96ff5034f9a9
aakashsingh9246 <34206017+aakashsingh9246@users.noreply.github.com>
2018-05-18 23:30:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+?>
+
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP Tutorial</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+</head>
+<body>
+    <p>Please Enter the number:</p>
+    <form method="post">
+        <input type="text" name="num">
+        <input type="submit" value="GO">
+</body>
+</html>
